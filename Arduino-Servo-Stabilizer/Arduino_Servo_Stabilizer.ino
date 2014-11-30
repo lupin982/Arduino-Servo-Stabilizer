209c56e9b0b167079c38e182b52849449c06d839
francesco.didomenico <lupin982@hotmail.com>
2014-11-30 23:35:45 +0100

    changed prject files

---
+++ Arduino-Servo-Stabilizer/Arduino_Servo_Stabilizer.ino
@@ -0,0 +1,20 @@
+/*
+ * Arduino_Servo_Stabilizer.ino
+ *
+ * Created: 11/30/2014 11:31:27 PM
+ * Author: Beatella
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
