4a20a6a497aadb5437700a996a0ef631cf698772
francesco.didomenico <lupin982@hotmail.com>
2014-12-01 21:56:46 +0100

    Added correct files

--- Visual Micro/.Arduino_Servo.vsarduino.h
+++ Visual Micro/.Arduino_Servo.vsarduino.h
@@ -5,15 +5,11 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Yún, Platform=avr, Package=arduino
+	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
-#define USB_VID 0x2341
-#define USB_PID 0x8041
-#define USB_MANUFACTURER 
-#define USB_PRODUCT "\"Arduino Yun\""
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
 #define ARDUINO 101
 #define ARDUINO_MAIN
 #define F_CPU 16000000L
@@ -21,6 +17,9 @@
 #define __cplusplus
 extern "C" void __cxa_pure_virtual() {;}
 
+//
+//
 
-#include "D:\Francesco\arduino-1.5.5-r2\hardware\arduino\avr\variants\yun\pins_arduino.h" 
+#include "D:\Francesco\arduino-1.5.5-r2\hardware\arduino\avr\variants\eightanaloginputs\pins_arduino.h" 
 #include "D:\Francesco\arduino-1.5.5-r2\hardware\arduino\avr\cores\arduino\arduino.h"
+#include "C:\Users\Beatella\Documents\Arduino\Arduino_Servo\Arduino_Servo.ino"
